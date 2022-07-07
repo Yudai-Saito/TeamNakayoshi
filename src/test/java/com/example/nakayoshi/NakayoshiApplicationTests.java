5717183a51c6b5a63fcacab3a8bf6d6a3018e382
YudaiSaito <yudai.saito.dev@gmail.com>
2022-07-07 22:28:21 +0900

    chore: SpringBootプロジェクトの立ち上げ
    顧客情報管理システムプロジェクト用SpringBootプロジェクト

---
+++ src/test/java/com/example/nakayoshi/NakayoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nakayoshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NakayoshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
