36ac3000930da44d3c4a02ab5e174e12c087d375
RyoyaFukushima <190146@jc-21.jp>
2022-07-12 12:28:04 +0900

    feat:顧客一覧画面のひな型

--- src/main/java/com/example/nakayoshi/NakayoshiApplication.java
+++ src/main/java/com/example/nakayoshi/NakayoshiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NakayoshiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NakayoshiApplication.class, args);
 	}
