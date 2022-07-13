8d8eddabd10a0a488f45cc2dba9d780060b32749
koji1325600 <te190088@gmail.com>
2022-07-13 11:02:21 +0900

    feat: CustomerController作成

---
+++ src/main/java/com/example/nakayoshi/CustomerController.java
@@ -0,0 +1,24 @@
+package com.example.nakayoshi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("customers")
+public class CustomerController {
+  @Autowired
+  CustomerService customerService;
+  @ModelAttribute 
+  CustomerForm setUpForm() {
+    return new CustomerForm();
+  }
+  @GetMapping
+  String list(Model model) {
+    model.addAttribute("customers", customerService.findAll());
+    return "customers/list";
+  }
+}
