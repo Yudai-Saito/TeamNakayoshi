5842ff81010cca0b9e014c984f54ab833f702dcc
koji1325600 <te190088@gmail.com>
2022-07-19 11:35:12 +0900

    feat: 詳細ボタンの機能を追加

--- src/main/java/com/example/nakayoshi/CustomerController.java
+++ src/main/java/com/example/nakayoshi/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.nakayoshi;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("customers")
@@ -29,5 +31,11 @@ public class CustomerController {
     return "redirect:customers";
   }	
 
+  @PostMapping(path = "detail", params = "form")
+  String detailForm(@RequestParam Integer id, CustomerForm form) {
+    CustomerForm customerForm = customerService.findOne(id);
+    BeanUtils.copyProperties(customerForm,  form);
+    return "customers/detail";
+  }
 
 }
