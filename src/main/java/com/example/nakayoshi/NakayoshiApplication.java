b38abf2806bb337b25f44a008bd8499819479e24
YudaiSaito <yudai.saito.dev@gmail.com>
2022-07-20 16:09:07 +0900

    feat: firebase admin sdkをプロジェクト内で読み込み

--- src/main/java/com/example/nakayoshi/NakayoshiApplication.java
+++ src/main/java/com/example/nakayoshi/NakayoshiApplication.java
@@ -1,13 +1,29 @@
 package com.example.nakayoshi;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication
 public class NakayoshiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileInputStream serviceAccount = new FileInputStream(Paths.get("").toAbsolutePath() + "\\src\\config\\serviceAccountKey.json");
+
+		FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
+						.build();
+
+		FirebaseApp.initializeApp(options);
+
 		SpringApplication.run(NakayoshiApplication.class, args);
 	}
-
 }
