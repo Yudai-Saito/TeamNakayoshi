87f9ba392c9f02a18f2efb4d8319ba9383452d32
koji1325600 <te190088@gmail.com>
2022-07-22 13:01:41 +0900

    feat: 詳細表示用の制約クラス作成

---
+++ src/main/java/com/example/nakayoshi/CustomerDetailsForm.java
@@ -0,0 +1,21 @@
+package com.example.nakayoshi;
+
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CustomerDetailsForm {
+
+  private Integer id ;
+  @NotNull
+  private Integer user_id;
+  @NotNull
+  private String detail;
+  @NotNull
+  private Date created_at;
+  private Date deleted_at;
+
+}
