793f5aaf45433d93f9540fd5bff4d980f5d5f41e
RyoyaFukushima <190146@jc-21.jp>
2022-08-24 11:29:41 +0900

    feat:ページわけロジックの書き直し

--- src/main/java/com/example/nakayoshi/CustomerController.java
+++ src/main/java/com/example/nakayoshi/CustomerController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.*;
+
 @Controller
 @RequestMapping("customers")
 public class CustomerController {
@@ -21,8 +23,28 @@ public class CustomerController {
   }
 
   @GetMapping
-  String list(Model model) {
-    model.addAttribute("customers", customerService.findAll());
-    return "customers/list";
+  String list(Model model, @RequestParam(defaultValue = "1") int page) {
+       FindforPagenateService findforPagenateService = new FindforPagenateService();
+       // 総表示数 
+       int totalListCnt = findforPagenateService.findAllCnt();
+
+       // 総表示数と現在ページ
+       Pagination pagination = new Pagination(totalListCnt, page);
+   
+       // DB接近スタートインデックス
+       int startIndex = pagination.getStartIndex();
+   
+       // ページことに表示する掲示物最大数
+       int pageSize = pagination.getPageSize();
+   
+       // 表示する要素の取得
+       List<CustomerBean> boardList = findforPagenateService.findListPaging(startIndex, pageSize);
+   
+       // モデルオブジェクトにオブジェクト格納
+      model.addAttribute("boardList", boardList);
+      model.addAttribute("pagination", pagination);
+      model.addAttribute("customers", customerService.findAll());
+
+      return "customers/list";
   }
 }
---
+++ src/main/java/com/example/nakayoshi/FindforPagenateService.java
@@ -0,0 +1,25 @@
+package com.example.nakayoshi;
+
+import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;;
+@Repository
+public class FindforPagenateService {
+    
+    @PersistenceContext
+    private EntityManager em;
+
+    public int findAllCnt() {
+        return ((Number) em.createQuery("select count(*) from Board")
+                    .getSingleResult()).intValue();
+    }
+
+    public List<CustomerBean> findListPaging(int startIndex, int pageSize) {
+        return em.createQuery("select b from Board b", CustomerBean.class)
+                    .setFirstResult(startIndex)
+                    .setMaxResults(pageSize)
+                    .getResultList();
+    }
+}
