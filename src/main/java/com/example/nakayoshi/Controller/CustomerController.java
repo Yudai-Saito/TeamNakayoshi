253849ebbd681fbd8d7fc6ccdef2af7009d54a61
koji1325600 <te190088@gmail.com>
2022-08-24 11:13:40 +0900

    feat: 日付の取得文を追加

--- src/main/java/com/example/nakayoshi/Controller/CustomerController.java
+++ src/main/java/com/example/nakayoshi/Controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.nakayoshi.Controller;
 
+import java.util.Date;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,14 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.nakayoshi.Form.CustomerDetailsForm;
 import com.example.nakayoshi.Form.CustomerForm;
 import com.example.nakayoshi.Service.CustomerService;
+import com.example.nakayoshi.Service.CustomerDetailsService;
 
 @Controller
 @RequestMapping("customers")
 public class CustomerController {
 
+  CustomerForm custForm = new CustomerForm();
+  CustomerDetailsForm custDetailsForm = new CustomerDetailsForm();
+
   @Autowired
   CustomerService customerService;
-   
+  CustomerDetailsService customerDetailsService;
+
   @ModelAttribute 
   CustomerForm setUpForm() {
     return new CustomerForm();
@@ -28,14 +34,15 @@ public class CustomerController {
   @GetMapping
   String list(Model model) {
     model.addAttribute("customers", customerService.findAll());
-    model.addAttribute("customers", customerService.findAllDetails());
     return "customers/list";
   }
 
   @PostMapping(path="create")
   String create(CustomerForm form, CustomerDetailsForm form2, Model mode) {
     customerService.create(form);
-    customerService.createDetails(form2);
+    customerDetailsService.createDetails(form2);
+    custForm.setCreated_at(new Date());
+    custDetailsForm.setCreated_at(new Date());
     return "redirect:customers";
   }	
 
@@ -43,7 +50,7 @@ public class CustomerController {
   String detailForm(@RequestParam Integer id, CustomerForm form, CustomerDetailsForm form2) {
     CustomerForm customerForm = customerService.findOne(id);
     BeanUtils.copyProperties(customerForm,  form);
-    CustomerDetailsForm customerDetailsForm = customerService.findOneDetails(id);
+    CustomerDetailsForm customerDetailsForm = customerDetailsService.findOneDetails(id);
     BeanUtils.copyProperties(customerDetailsForm,  form2);
     return "customers/detail";
   }
@@ -58,6 +65,7 @@ public class CustomerController {
   @PostMapping(path = "edit")
   String edit(@RequestParam Integer id, CustomerForm form) {
     customerService.update(form);
+    custForm.setUpdated_at(new Date());
     return "redirect:/customers";
   }
 
