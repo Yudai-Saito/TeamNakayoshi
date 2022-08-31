565f263bbbc4c79f7dc3a8e6fb56fa62d9846a1e
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-08-31 09:31:49 +0900

    feat: コントローラから初回問い合わせ内容を受け取り格納

--- src/main/java/com/example/nakayoshi/Service/CustomerDetailsService.java
+++ src/main/java/com/example/nakayoshi/Service/CustomerDetailsService.java
@@ -1,11 +1,16 @@
 package com.example.nakayoshi.Service;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
 import com.example.nakayoshi.Bean.CustomerDetailsBean;
 import com.example.nakayoshi.Form.CustomerDetailsForm;
+import com.example.nakayoshi.Form.CustomerForm;
 import com.example.nakayoshi.Repository.CustomerDetailsRepository;
 
 @Service
@@ -13,13 +18,26 @@ public class CustomerDetailsService {
   @Autowired
   CustomerDetailsRepository customerDetailsRepository;
 
-  public CustomerDetailsForm createDetails(CustomerDetailsForm customerDetailsForm) {
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+
+  public CustomerDetailsForm createDetail(CustomerForm customerForm, CustomerDetailsForm customerDetailsForm) {
 
     CustomerDetailsBean customerDetailsBean = new CustomerDetailsBean();
+
+    Date now = new Date();
+    customerDetailsForm.setCreated_at(now);
+
+    String sql = "select * from customers where phone_number = ?";
+
+    Map<String, Object> user = jdbcTemplate.queryForMap(sql, customerForm.getPhone_number());
+    customerDetailsForm.setUser_id((Integer)user.get("id"));
+
     BeanUtils.copyProperties(customerDetailsForm, customerDetailsBean);
     customerDetailsRepository.save(customerDetailsBean);
-    return customerDetailsForm;
 
+    return customerDetailsForm;
   }
 
   public CustomerDetailsForm updateDetails(CustomerDetailsForm customerDetailsForm) {
@@ -27,8 +45,8 @@ public class CustomerDetailsService {
     CustomerDetailsBean customerDetailsBean = new CustomerDetailsBean();
     BeanUtils.copyProperties(customerDetailsForm, customerDetailsBean);
     customerDetailsRepository.save(customerDetailsBean);
+
     return customerDetailsForm;
-    
   }
 
   public CustomerDetailsForm findOneDetails(Integer id) {
@@ -36,7 +54,7 @@ public class CustomerDetailsService {
     Optional<CustomerDetailsBean> customerDetailsBean = customerDetailsRepository.findById(id);
     CustomerDetailsForm customerDetailsForm = new CustomerDetailsForm();
     BeanUtils.copyProperties(customerDetailsBean, customerDetailsForm);
-    return customerDetailsForm;
 
+    return customerDetailsForm;
   }
 }
