b4eec93c9ba5e4cb87d659c466437064b2ea138c
YudaiSaito <yudai.saito.dev@gmail.com>
2022-09-05 01:16:25 +0900

    feat: 問い合わせ追記ルーティングと追記処理

--- src/main/java/com/example/nakayoshi/Controller/CustomerController.java
+++ src/main/java/com/example/nakayoshi/Controller/CustomerController.java
@@ -63,9 +63,18 @@ public class CustomerController {
   @GetMapping("/form/{user_id}")
   public String postScript(@PathVariable int user_id, Model model) {
     model.addAttribute("customer", customerService.findOne(user_id));
-    
+    model.addAttribute("detail", customerDetailService.findUserDetails(user_id));
+
     return "forms/postscript";
   }
+
+  @PostMapping(path = "add", params = "user_id")
+  String addDetails(@RequestParam Integer user_id, @RequestParam String detail, Model model){
+    customerDetailService.addDetails(user_id, detail);
+
+    return "redirect:/customers/" + user_id;
+  }
+
 
   @PostMapping(path = "edit", params = "form")
   String editForm(@RequestParam Integer id, CustomerForm form) {
